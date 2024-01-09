862389e8a4369cb1de9743b6d130a546bc8db8e4
anurag <anuragss2311@gmail.com>
2024-01-09 18:35:01 +0530

    Loaded tranformed data into database

--- dags/ETL-API-To-SQL-Server.py
+++ dags/ETL-API-To-SQL-Server.py
@@ -7,9 +7,8 @@ import pandas as pd
 # security code that will return key value pairs.
 from bsedata.bse import BSE
 import time
-import pyodbc # for sql queries
-from sqlalchemy import create_engine, inspect # pre-built toolkit to work with sql database.
-from sqlalchemy.exc import SQLAlchemyError
+import psycopg2 # for sql queries
+from sqlalchemy import create_engine, inspect# pre-built toolkit to work with sql database.
 import sqlalchemy as sa
 import logging
 
@@ -17,13 +16,14 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 1000)
 
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': True,
-    'start_date': datetime(2023, 12, 1),
+    'start_date': datetime(2024, 1, 4),
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 2,
+    'retries': 0,
     'retry_delay': timedelta(minutes=1),
 }
 
@@ -38,9 +38,7 @@ dag = DAG(
 def extract_data(**kwargs):
     excel_url = "https://raw.githubusercontent.com/jangid6/Stock-ETL-Project/main/Equity.xlsx"  
     EquityDF = pd.read_excel(excel_url, engine='openpyxl')
-    print("Reading of the data from API is successful")
     logging.info("Reading of the data from API is successful")
-    print(EquityDF.head(n=2))
     logging.info(len(EquityDF))
 
 # CALLING BSE API for fetching Stocks Data for eg: Price, Code, Updated Date, Open Price, Close Price, Mrkt Cap
@@ -63,23 +61,22 @@ def extract_data(**kwargs):
 
     # Here I'm filtering EquityDF because I don't need 4000. I just need 50, so i'm filtering by using isin().
     nifty50_OnlyDF= EquityDF[EquityDF['Security Id'].isin(listOf_Nifty50_StockIDs)].reset_index(drop=True)
-    print("EquityDF:>>>>",(len(nifty50_OnlyDF)))
     logging.info(len(nifty50_OnlyDF))
+    nifty50_OnlyDF.rename(columns={'Group': 'CompanyGroup'}, inplace=True)
     nifty50_OnlyDF.columns = nifty50_OnlyDF.columns.str.replace(' ', '')
     nifty50_OnlyDF.head()
-    nifty50_OnlyDF['SecurityCode'].values
+
     # creating Bse Lib Object
     bseObject = BSE(update_codes=True)
-    listof_stockDicts = []
+    result_dfs = []
     sqcode_ListNifty50 = nifty50_OnlyDF['SecurityCode'].values
     
     for sqCode in sqcode_ListNifty50:
         try:
-            # stockDict is the item that is returned from the API. This is API return data.
-            stockDict = bseObject.getQuote(sqCode) # return key-value pair
-            stockDict.pop("buy", None)
-            stockDict.pop("sell", None)
-            listof_stockDicts.append(stockDict)
+            # stock_Data is the item that is returned from the API. This is API return data.
+            stock_Data = bseObject.getQuote(sqCode) # return key-value pair
+            stock_df = pd.DataFrame([stock_Data])
+            result_dfs.append(stock_df)
             # Introduce a small delay between API calls to their server. This precaution helps
             # prevent server issues and ensures a smoother interaction with their network.
             time.sleep(0.5) # Note:- time delay to avoid potential issues with the Bombay Stock Exchange (BSE) blocking my
@@ -87,15 +84,20 @@ def extract_data(**kwargs):
         except IndexError:
             logging.error(f"IndexError for {sqCode}: Data not available")
 
-    nifty50DailyTable = pd.DataFrame(listof_stockDicts)
+    nifty50DailyTable = pd.concat(result_dfs, ignore_index=True).iloc[:,:-2]
     nifty50DailyTable.head()
-    # We can store nifty50DailyTable in XCom for use in the next task
-    kwargs['ti'].xcom_push(key='nifty50_data', value=nifty50DailyTable)
+    # Convert the DataFrame to a JSON-serializable format
+    nifty50_data_dict = nifty50DailyTable.to_dict(orient='records')
+    # Push the JSON-serializable data to XCom
+    kwargs['ti'].xcom_push(key='nifty50_data', value=nifty50_data_dict)
 
 # Data Pre-Processing (Cleaning and Transformation) for Microsoft SQL Server Database
 def transform_data(**kwargs):
-    task_instance = kwargs['ti']
-    nifty50DailyTable = task_instance.xcom_pull(task_ids='extract_data', key='nifty50_data')
+    task_instance = kwargs.get('ti')
+    nifty50_data_dict = task_instance.xcom_pull(task_ids='extract_data', key='nifty50_data')
+    logging.info("XCom Value: %s", nifty50_data_dict)
+    # Convert the JSON-serializable data back to a DataFrame
+    nifty50DailyTable = pd.DataFrame.from_records(nifty50_data_dict)
     # Here, I'm renaming the column because 'group' is a reserved keyword in SQL queries,  
     # and I also prefer using descriptive alphabetic names instead of alphanumeric ones.
 
@@ -104,116 +106,127 @@ def transform_data(**kwargs):
     nifty50DailyTable.rename(columns={'52weekHigh': 'fiftytwoweekHigh'}, inplace=True)
     nifty50DailyTable.rename(columns={'52weekLow': 'fiftytwoweekLow'}, inplace=True)
     nifty50DailyTable.rename(columns={'2WeekAvgQuantity': 'twoWeekAvgQuantity'}, inplace=True)
-    # In Existing dataframe column i.e: updateOn. It's not really a correct date format
-    nifty50DailyTable['updatedOn'] = pd.to_datetime(nifty50DailyTable['updatedOn'], format='%d %b %y | %I:%M %p', errors='coerce')
+    nifty50DailyTableTest_DF = nifty50DailyTable.copy()
+    # In Existing dataframe column i.e: updateOn. It's not really a correct date format. 
+    # Convert 'updatedOn' column to datetime and extract date
+    nifty50DailyTableTest_DF['updatedOn'] = pd.to_datetime(nifty50DailyTableTest_DF['updatedOn'], format='%d %b %y | %I:%M %p', errors='coerce')
 
     # Check if there are any invalid or missing date values.
-    if pd.isna(nifty50DailyTable['updatedOn']).any():
+    if pd.isna(nifty50DailyTableTest_DF['updatedOn']).any():
         logging.warning("There are invalid or missing date values in the 'updatedOn' column.")
     else:
         # Extract data from 'updateOn' column & convert the column to datetime.
-        nifty50DailyTable['updatedOn'] = pd.to_datetime(nifty50DailyTable['updatedOn'].dt.date)
+        nifty50DailyTableTest_DF['updatedOn'] = pd.to_datetime(nifty50DailyTableTest_DF['updatedOn'].dt.date)
+
+    # Convert Timestamp objects to strings
+    nifty50DailyTableTest_DF['updatedOn'] = nifty50DailyTableTest_DF['updatedOn'].astype(str)
 
     # Data-Cleaning part
-    if 'totalTradedValueCr' not in nifty50DailyTable.columns:
-        nifty50DailyTable['totalTradedValueCr'] = pd.to_numeric(nifty50DailyTable['totalTradedValue'].str.replace(',', '').str.replace(' Cr.', '', regex=True), errors='coerce')
-        nifty50DailyTable['totalTradedQuantityLakh'] = pd.to_numeric(nifty50DailyTable['totalTradedQuantity'].str.replace(',', '').str.replace(' Lakh', '', regex=True), errors='coerce')
-        nifty50DailyTable['twoWeekAvgQuantityLakh'] = pd.to_numeric(nifty50DailyTable['twoWeekAvgQuantity'].str.replace(',', '').str.replace(' Lakh', '', regex=True), errors='coerce')
-        nifty50DailyTable['marketCapFullCr'] = pd.to_numeric(nifty50DailyTable['marketCapFull'].str.replace(',', '').str.replace(' Cr.', '', regex=True), errors='coerce')
-        nifty50DailyTable['marketCapFreeFloatCr'] = pd.to_numeric(nifty50DailyTable['marketCapFreeFloat'].str.replace(',', '').str.replace(' Cr.', '', regex=True), errors='coerce')
+    if 'totalTradedValueCr' not in nifty50DailyTableTest_DF.columns:
+        nifty50DailyTableTest_DF['totalTradedValueCr'] = pd.to_numeric(nifty50DailyTableTest_DF['totalTradedValue'].str.replace(',', '').str.replace(' Cr.', '', regex=True), errors='coerce')
+        nifty50DailyTableTest_DF['totalTradedQuantityLakh'] = pd.to_numeric(nifty50DailyTableTest_DF['totalTradedQuantity'].str.replace(',', '').str.replace(' Lakh', '', regex=True), errors='coerce')
+        nifty50DailyTableTest_DF['twoWeekAvgQuantityLakh'] = pd.to_numeric(nifty50DailyTableTest_DF['twoWeekAvgQuantity'].str.replace(',', '').str.replace(' Lakh', '', regex=True), errors='coerce')
+        nifty50DailyTableTest_DF['marketCapFullCr'] = pd.to_numeric(nifty50DailyTableTest_DF['marketCapFull'].str.replace(',', '').str.replace(' Cr.', '', regex=True), errors='coerce')
+        nifty50DailyTableTest_DF['marketCapFreeFloatCr'] = pd.to_numeric(nifty50DailyTableTest_DF['marketCapFreeFloat'].str.replace(',', '').str.replace(' Cr.', '', regex=True), errors='coerce')
 
-        nifty50DailyTable.drop(['totalTradedValue', 'totalTradedQuantity','twoWeekAvgQuantity', 'marketCapFull', 'marketCapFreeFloat'], axis=1, inplace=True)
+        nifty50DailyTableTest_DF.drop(['totalTradedValue', 'totalTradedQuantity','twoWeekAvgQuantity', 'marketCapFull', 'marketCapFreeFloat'], axis=1, inplace=True)
+    
+    # Convert the DataFrame to a JSON-serializable format
+    nifty50_data_dict_transformed = nifty50DailyTableTest_DF.to_dict(orient='records')
+    
+    # Push the JSON-serializable data to XCom
+    kwargs['ti'].xcom_push(key='nifty50_data_transformed', value=nifty50_data_dict_transformed)
+    
+    
 
-    nifty50DailyTable.head()
-
-# Loading data to Microsoft SQL Server Database Using pyodbc connection and Sqlalchemy Engine
+# Loading data to PostgreSQL Database Using psycopg2 connection and Sqlalchemy Engine
 def load_data_to_sql_server(**kwargs):
-    task_instance = kwargs['ti']
-    nifty50DailyTable = task_instance.xcom_pull(task_ids='transform_data', key='nifty50_data')
+    task_instance = kwargs.get('ti')
+    nifty50DailyTableTest_data = task_instance.xcom_pull(task_ids='transform_data', key='nifty50_data_transformed')
+    logging.info("XCom Value: %s", nifty50DailyTableTest_data)
 
-    server = 'DESKTOP-A066HIG\SQLEXPRESS'
+    nifty50DailyTableTest_DF=pd.DataFrame.from_records(nifty50DailyTableTest_data)
+    nifty50DailyTableTest_DF['updatedOn'] = pd.to_datetime(nifty50DailyTableTest_DF['updatedOn'])
+
+    logging.info("Original Column Names: %s", nifty50DailyTableTest_DF.columns)
+
     database = 'nifty50'
-    username = 'sa'
-    password = 'sqlserver'
-    driver = 'ODBC Driver 17 for SQL Server'
+    username = 'postgres'
+    password = 'Anuragss07#'
     
-    # SQL database connection string
-    conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    # PostgreSQL database connection string
+    conn_str = f'postgresql://{username}:{password}@localhost:5432/{database}'
+    logging.info("conn_str", conn_str)
     
-    # Create an SQLAlchemy Engine
-    engine = create_engine(f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}")
-
-    def create_connection(conn_str):
-        conn = pyodbc.connect(conn_str)
-        cursor = conn.cursor()
-        return conn, cursor
 
     try:
-        # Try to connect to the SQL Server using the engine
-        connection = engine.connect()
-        print("Connection successful!")
+        # Try to connect to the PostgreSQL using the engine
+        conn=psycopg2.connect(conn_str)
         logging.info("Connection successful!")
-        connection.close()
-        conn,  cursor = create_connection(conn_str)
+        cursor = conn.cursor()
+        engine = create_engine(conn_str, echo=True)
+
+        # Create an inspector
         inspector = inspect(engine)
         nifty50_table_name = 'nifty50_dailydata'
         if not inspector.has_table(nifty50_table_name):
             nifty50_table_schema = f'''
             CREATE TABLE {nifty50_table_name} (
-                companyName NVARCHAR(MAX),
-                currentValue FLOAT,
-                change FLOAT,
-                pChange FLOAT,
-                updatedOn DATE,
-                securityID NVARCHAR(MAX),
-                scripCode NVARCHAR(MAX),
-                sharegroup NVARCHAR(MAX),
-                faceValue FLOAT,
-                industry NVARCHAR(MAX),
-                previousClose FLOAT,
-                previousOpen FLOAT,
-                dayHigh FLOAT,
-                dayLow FLOAT,
-                fiftytwoweekHigh FLOAT,
-                fiftytwoweekLow FLOAT,
-                weightedAvgPrice FLOAT,
-                totalTradedQuantityLakh FLOAT,
-                totalTradedValueCr FLOAT,
-                twoWeekAvgQuantityLakh FLOAT,
-                marketCapFullCr FLOAT,
-                marketCapFreeFloatCr FLOAT
+                "companyName" VARCHAR,
+                "currentValue" NUMERIC,
+                "change" NUMERIC,
+                "pChange" NUMERIC,
+                "updatedOn" TIMESTAMP,
+                "securityID" VARCHAR,
+                "scripCode" VARCHAR,
+                "sharegroup" VARCHAR,
+                "faceValue" NUMERIC,
+                "industry" VARCHAR,
+                "previousClose" NUMERIC,
+                "previousOpen" NUMERIC,
+                "dayHigh" NUMERIC,
+                "dayLow" NUMERIC,
+                "fiftytwoweekHigh" NUMERIC,
+                "fiftytwoweekLow" NUMERIC,
+                "weightedAvgPrice" NUMERIC,
+                "totalTradedValueCr" NUMERIC,
+                "totalTradedQuantityLakh" NUMERIC,
+                "twoWeekAvgQuantityLakh" NUMERIC,
+                "marketCapFullCr" NUMERIC,
+                "marketCapFreeFloatCr" NUMERIC
             );
             '''
             # Execute the schema to create the table
             cursor.execute(nifty50_table_schema)
             conn.commit()
+            cursor.close()
             conn.close()
+        
+        logging.info("DataFrame Content: %s", nifty50DailyTableTest_DF.head())
+        logging.info("DataFrame Data Types: %s", nifty50DailyTableTest_DF.dtypes)
 
         with engine.begin() as engineConn:
             # Check the maximum 'updatedOn' date in the existing SQL table
-            sql_max_updatedOn = pd.read_sql_query(sa.text(f'SELECT MAX(updatedOn) FROM {nifty50_table_name}'), engineConn).iloc[0, 0]
+            sql_max_updatedOn = pd.read_sql_query(sa.text(f'SELECT MAX("updatedOn") FROM {nifty50_table_name}'), engineConn).iloc[0, 0]
             logging.info(sql_max_updatedOn)
-            print(sql_max_updatedOn)
+            
             # Check the maximum 'updatedOn' date in the DataFrame
-            df_max_updatedOn = nifty50DailyTable['updatedOn'].max()
-            logging.info(df_max_updatedOn)
-            print(df_max_updatedOn)
+            if nifty50DailyTableTest_DF is not None:
+                df_max_updatedOn = nifty50DailyTableTest_DF['updatedOn'].max()
+                logging.info(df_max_updatedOn)
 
             # Compare the dates and decide whether to append new data
             if (pd.isnull(sql_max_updatedOn)) and (not pd.isnull(df_max_updatedOn)):
-                nifty50DailyTable.to_sql(nifty50_table_name, engine, index=False, if_exists='append', method='multi')
-                logging.info("Daily Data didn't exist, but now inserted successfully.")
-                print("Daily Data didn't exist, but now inserted successfully.")
+                nifty50DailyTableTest_DF.to_sql(nifty50_table_name, engine, index=False, if_exists='append', method='multi')
+                logging.info("Daily Data didn't exist, but now inserted succesDFully.")
             else:
                 if (df_max_updatedOn > pd.Timestamp(sql_max_updatedOn)):
-                    nifty50DailyTable.to_sql(nifty50_table_name, engine, index=False, if_exists='append', method='multi')
+                    nifty50DailyTableTest_DF.to_sql(nifty50_table_name, engine, index=False, if_exists='append', method='multi')
                     logging.info("Data appended successfully.")
-                    print("Data appended successfully.")
                 else:
                     logging.info("No new data to append")
-                    print("No new data to append.")
-    except SQLAlchemyError as e:
-        logging.error(f"Error connecting to SQL Server: {e}")
+    except psycopg2.Error as e:
+        logging.error(f"Error connecting to PostgreSQL database: {e}")
 
 # Define tasks in the DAG
 extract_task = PythonOperator(
